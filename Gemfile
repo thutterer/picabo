3032a57fecab1f0f526242ef588c68919b40c907
Thomas Hutterer <tohu@tuta.io>
2021-11-20 14:37:07 +0100

    Setup Materialize and Haml

--- Gemfile
+++ Gemfile
@@ -5,3 +5,6 @@ gem "ghost_train", github: "thutterer/ghost_train"
 gem "app", path: "."
 
 # Add your gems here
+
+gem "haml"
+gem "materialize-sass", "~> 1.0.0"
--- lib/app.rb
+++ lib/app.rb
@@ -3,19 +3,12 @@ require "app/engine"
 module App
   def self.config
     {
-      name: "My Awesome Application",
-      short_name: "My App",
-      description: "Young and wild.",
+      name: "Picabo",
+      short_name: "Picabo",
+      description: "Share photos with friends and family.",
 
-      theme_color: "#64b5f6",
-      text_color: "#f7f7f7",
-      secondary_text_color: "#aaa",
-      background_color: "#18181b",
-      background_color_secondary: "#202022",
-      accent_color: "#0097fc4f",
-      border_radius: "5px"
+      theme_color: "#ab47bc",
+      background_color: "#fff"
     }
   end
-
-  # Your code goes here...
 end
